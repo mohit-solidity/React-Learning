e5b9c63697250dd3c4b23ac9c9a8b03b16451476
Mohit <mohit96046@gmail.com>
2026-09-15 00:31:39 +0530

    Add User Function

--- components/Creators.jsx
+++ components/Creators.jsx
@@ -7,10 +7,11 @@ export default function CreatorsData(){
         { id: 3, name: 'Gourav', subscribers: 80, subscribed: false },
     ];
     const [creatorsData, setCreatorData] = useState(initialCreators);
+    const [creatorName, setCreatorName] = useState("");
 
     const setToggle = (id) => {
         setCreatorData((currentCreators)=>
-            currentCreators.map((creator)=>{
+                currentCreators.map((creator)=>{
                 if(creator.id!==id) return creator;
                 const subscribed = !creator.subscribed;
                 return{
@@ -22,17 +23,36 @@ export default function CreatorsData(){
         )
     }
 
+    const addCreator = (event) =>{
+        event.preventDefault();
+        if(!creatorName) return;
+        setCreatorData((currentCreators)=>[
+            ...currentCreators,
+            {
+                id: currentCreators.length + 1,
+                name:creatorName,
+                subscribers:0,
+                subscribed:false
+            }
+        ])
+        setCreatorName("");
+    }
+
     return(
         <>
         {
             creatorsData.map((creator)=>(
-                <div style={{border:'2px solid green', margin:'3px'}}>
+                <div style={{border:'2px solid green', margin:'3px'}} key={creator.id}>
                     <p>Craetor Name : {creator.name}</p>
                     <p>Subscribers : {creator.subscribers}</p>
                     <button onClick={()=>setToggle(creator.id)}>{creator.subscribed?"UnSubscribe":"Subscribe"}</button>
                 </div>
             ))
         }
+        <form onSubmit={addCreator}>
+            <input type="text" placeholder="Enter Creator's Name" onChange={(e)=>setCreatorName(e.target.value)} required />
+            <button type="submit">Add</button>
+        </form>
         </>
     )
 }
