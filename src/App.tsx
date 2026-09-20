10d1d020f3c6c54e86f2695d3d8aaa97b9f3d00e
Mohit <mohit96046@gmail.com>
2026-09-20 13:10:28 +0530

    Make them outside so it can renders fully

--- src/App.tsx
+++ src/App.tsx
@@ -9,14 +9,14 @@ function App(){
   return(
     <>
       <h1>Hey Guys</h1>
+      <ConnectButton label="Sign In To Get Started" showBalance={false} />
+      <CreatorsData />
       {isConnected&&(
         <>
           <p>Address : {address}</p>
           <p>Chain Id : {chainId}</p>
           <p>Chain Name : {chain?.name}</p>
           <Test name={"Mohit"} />
-          <ConnectButton label="Sign In To Get Started" showBalance={false} />
-          <CreatorsData />
         </>
       )}
     </>
